6e27de0c85727729aca6c41d1bb923dd4ac72219
Ic-arbon <dty2015@hotmail.com>
2025-11-10 11:31:18 +0800

    fix(patch): align manual module with generated style

--- src/sf32lb52x/patch.rs
+++ src/sf32lb52x/patch.rs
@@ -13,46 +13,38 @@ impl Patch {
     pub const fn as_ptr(&self) -> *mut () {
         self.ptr as _
     }
-    #[doc = "Channel register；支持索引 0..31"]
     #[inline(always)]
-    pub const fn ch(self, idx: usize) -> crate::common::Reg<regs::Ch, crate::common::RW> {
-        assert!(idx < 32usize);
-        unsafe { crate::common::Reg::from_ptr(self.ptr.add(idx * 4usize) as _) }
+    pub const fn ch(self, n: usize) -> crate::common::Reg<regs::Ch, crate::common::RW> {
+        assert!(n < 32usize);
+        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x0usize + n * 4usize) as _) }
     }
-    #[doc = "Channel Enable Register"]
     #[inline(always)]
     pub const fn cer(self) -> crate::common::Reg<regs::Cer, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.add(0x80usize) as _) }
+        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x80usize) as _) }
     }
-    #[doc = "Channel Status Register"]
     #[inline(always)]
     pub const fn csr(self) -> crate::common::Reg<regs::Csr, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.add(0x84usize) as _) }
+        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x84usize) as _) }
     }
-    #[doc = "Channel Data Register"]
     #[inline(always)]
     pub const fn cdr(self) -> crate::common::Reg<regs::Cdr, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.add(0x88usize) as _) }
+        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x88usize) as _) }
     }
-    #[doc = "Interrupt Enable Register"]
     #[inline(always)]
     pub const fn ier(self) -> crate::common::Reg<regs::Ier, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.add(0x90usize) as _) }
+        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x90usize) as _) }
     }
-    #[doc = "Interrupt Status Register"]
     #[inline(always)]
     pub const fn isr(self) -> crate::common::Reg<regs::Isr, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.add(0x94usize) as _) }
+        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x94usize) as _) }
     }
-    #[doc = "Interrupt Clear Register"]
     #[inline(always)]
     pub const fn icr(self) -> crate::common::Reg<regs::Icr, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.add(0x98usize) as _) }
+        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x98usize) as _) }
     }
-    #[doc = "Version Register"]
     #[inline(always)]
     pub const fn ver(self) -> crate::common::Reg<regs::Ver, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.add(0x9cusize) as _) }
+        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x9cusize) as _) }
     }
 }
 pub mod regs;
--- src/sf32lb52x/patch/regs.rs
+++ src/sf32lb52x/patch/regs.rs
@@ -1,15 +1,16 @@
-#[doc = "补丁通道寄存器"]
+#[doc = "Patch channel register"]
 #[repr(transparent)]
 #[derive(Copy, Clone, Eq, PartialEq)]
 pub struct Ch(pub u32);
 impl Ch {
     const ADDR_MASK: u32 = 0x1ffff << 2;
-    #[doc = "补丁入口地址字段"]
+    #[doc = "Breakpoint entry address (bits 18:2)"]
+    #[must_use]
     #[inline(always)]
     pub const fn addr(&self) -> u32 {
         (self.0 & Self::ADDR_MASK) >> 2
     }
-    #[doc = "设置补丁入口地址字段"]
+    #[doc = "Set breakpoint entry address (bits 18:2)"]
     #[inline(always)]
     pub fn set_addr(&mut self, val: u32) {
         self.0 = (self.0 & !Self::ADDR_MASK) | ((val & 0x1ffff) << 2);
@@ -34,51 +35,261 @@ impl defmt::Format for Ch {
         defmt::write!(f, "Ch {{ addr: {=u32:?} }}", self.addr());
     }
 }
-macro_rules! simple_reg {
-    ($name:ident, $doc:literal) => {
-        #[doc = $doc]
-        #[repr(transparent)]
-        #[derive(Copy, Clone, Eq, PartialEq)]
-        pub struct $name(pub u32);
-        impl $name {
-            #[inline(always)]
-            pub const fn bits(&self) -> u32 {
-                self.0
-            }
-            #[inline(always)]
-            pub fn set_bits(&mut self, val: u32) {
-                self.0 = val;
-            }
-        }
-        impl Default for $name {
-            #[inline(always)]
-            fn default() -> $name {
-                $name(0)
-            }
-        }
-        impl core::fmt::Debug for $name {
-            fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-                f.debug_struct(stringify!($name))
-                    .field("bits", &format_args!("0x{:08x}", self.0))
-                    .finish()
-            }
-        }
-        #[cfg(feature = "defmt")]
-        impl defmt::Format for $name {
-            fn format(&self, f: defmt::Formatter) {
-                defmt::write!(
-                    f,
-                    "Reg {{ bits: {=u32:#010x} }}",
-                    self.0
-                );
-            }
-        }
-    };
+#[doc = "Patch channel enable register"]
+#[repr(transparent)]
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct Cer(pub u32);
+impl Cer {
+    #[doc = "Channel enable bitmask"]
+    #[must_use]
+    #[inline(always)]
+    pub const fn ce(&self) -> u32 {
+        self.0
+    }
+    #[doc = "Set channel enable bitmask"]
+    #[inline(always)]
+    pub fn set_ce(&mut self, val: u32) {
+        self.0 = val;
+    }
 }
-simple_reg!(Cer, "通道使能寄存器");
-simple_reg!(Csr, "通道状态寄存器");
-simple_reg!(Cdr, "通道数据寄存器");
-simple_reg!(Ier, "中断使能寄存器");
-simple_reg!(Isr, "中断状态寄存器");
-simple_reg!(Icr, "中断清除寄存器");
-simple_reg!(Ver, "模块版本寄存器");
+impl Default for Cer {
+    #[inline(always)]
+    fn default() -> Cer {
+        Cer(0)
+    }
+}
+impl core::fmt::Debug for Cer {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        f.debug_struct("Cer")
+            .field("ce", &format_args!("0x{:08x}", self.ce()))
+            .finish()
+    }
+}
+#[cfg(feature = "defmt")]
+impl defmt::Format for Cer {
+    fn format(&self, f: defmt::Formatter) {
+        defmt::write!(f, "Cer {{ ce: {=u32:#010x} }}", self.ce());
+    }
+}
+
+#[doc = "Patch channel status register"]
+#[repr(transparent)]
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct Csr(pub u32);
+impl Csr {
+    #[doc = "Channel status bitmask"]
+    #[must_use]
+    #[inline(always)]
+    pub const fn cs(&self) -> u32 {
+        self.0
+    }
+    #[doc = "Set channel status bitmask"]
+    #[inline(always)]
+    pub fn set_cs(&mut self, val: u32) {
+        self.0 = val;
+    }
+}
+impl Default for Csr {
+    #[inline(always)]
+    fn default() -> Csr {
+        Csr(0)
+    }
+}
+impl core::fmt::Debug for Csr {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        f.debug_struct("Csr")
+            .field("cs", &format_args!("0x{:08x}", self.cs()))
+            .finish()
+    }
+}
+#[cfg(feature = "defmt")]
+impl defmt::Format for Csr {
+    fn format(&self, f: defmt::Formatter) {
+        defmt::write!(f, "Csr {{ cs: {=u32:#010x} }}", self.cs());
+    }
+}
+
+#[doc = "Patch channel data register"]
+#[repr(transparent)]
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct Cdr(pub u32);
+impl Cdr {
+    #[doc = "Channel data payload"]
+    #[must_use]
+    #[inline(always)]
+    pub const fn data(&self) -> u32 {
+        self.0
+    }
+    #[doc = "Set channel data payload"]
+    #[inline(always)]
+    pub fn set_data(&mut self, val: u32) {
+        self.0 = val;
+    }
+}
+impl Default for Cdr {
+    #[inline(always)]
+    fn default() -> Cdr {
+        Cdr(0)
+    }
+}
+impl core::fmt::Debug for Cdr {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        f.debug_struct("Cdr")
+            .field("data", &format_args!("0x{:08x}", self.data()))
+            .finish()
+    }
+}
+#[cfg(feature = "defmt")]
+impl defmt::Format for Cdr {
+    fn format(&self, f: defmt::Formatter) {
+        defmt::write!(f, "Cdr {{ data: {=u32:#010x} }}", self.data());
+    }
+}
+
+#[doc = "Patch interrupt enable register"]
+#[repr(transparent)]
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct Ier(pub u32);
+impl Ier {
+    #[doc = "Channel interrupt enable mask"]
+    #[must_use]
+    #[inline(always)]
+    pub const fn cie(&self) -> u32 {
+        self.0
+    }
+    #[doc = "Set channel interrupt enable mask"]
+    #[inline(always)]
+    pub fn set_cie(&mut self, val: u32) {
+        self.0 = val;
+    }
+}
+impl Default for Ier {
+    #[inline(always)]
+    fn default() -> Ier {
+        Ier(0)
+    }
+}
+impl core::fmt::Debug for Ier {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        f.debug_struct("Ier")
+            .field("cie", &format_args!("0x{:08x}", self.cie()))
+            .finish()
+    }
+}
+#[cfg(feature = "defmt")]
+impl defmt::Format for Ier {
+    fn format(&self, f: defmt::Formatter) {
+        defmt::write!(f, "Ier {{ cie: {=u32:#010x} }}", self.cie());
+    }
+}
+
+#[doc = "Patch interrupt status register"]
+#[repr(transparent)]
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct Isr(pub u32);
+impl Isr {
+    #[doc = "Channel interrupt status bits"]
+    #[must_use]
+    #[inline(always)]
+    pub const fn cis(&self) -> u32 {
+        self.0
+    }
+    #[doc = "Set channel interrupt status bits"]
+    #[inline(always)]
+    pub fn set_cis(&mut self, val: u32) {
+        self.0 = val;
+    }
+}
+impl Default for Isr {
+    #[inline(always)]
+    fn default() -> Isr {
+        Isr(0)
+    }
+}
+impl core::fmt::Debug for Isr {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        f.debug_struct("Isr")
+            .field("cis", &format_args!("0x{:08x}", self.cis()))
+            .finish()
+    }
+}
+#[cfg(feature = "defmt")]
+impl defmt::Format for Isr {
+    fn format(&self, f: defmt::Formatter) {
+        defmt::write!(f, "Isr {{ cis: {=u32:#010x} }}", self.cis());
+    }
+}
+
+#[doc = "Patch interrupt clear register"]
+#[repr(transparent)]
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct Icr(pub u32);
+impl Icr {
+    #[doc = "Channel interrupt clear bits"]
+    #[must_use]
+    #[inline(always)]
+    pub const fn cic(&self) -> u32 {
+        self.0
+    }
+    #[doc = "Set channel interrupt clear bits"]
+    #[inline(always)]
+    pub fn set_cic(&mut self, val: u32) {
+        self.0 = val;
+    }
+}
+impl Default for Icr {
+    #[inline(always)]
+    fn default() -> Icr {
+        Icr(0)
+    }
+}
+impl core::fmt::Debug for Icr {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        f.debug_struct("Icr")
+            .field("cic", &format_args!("0x{:08x}", self.cic()))
+            .finish()
+    }
+}
+#[cfg(feature = "defmt")]
+impl defmt::Format for Icr {
+    fn format(&self, f: defmt::Formatter) {
+        defmt::write!(f, "Icr {{ cic: {=u32:#010x} }}", self.cic());
+    }
+}
+
+#[doc = "Patch version register"]
+#[repr(transparent)]
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct Ver(pub u32);
+impl Ver {
+    #[doc = "Module version identifier"]
+    #[must_use]
+    #[inline(always)]
+    pub const fn id(&self) -> u32 {
+        self.0
+    }
+    #[doc = "Set module version identifier"]
+    #[inline(always)]
+    pub fn set_id(&mut self, val: u32) {
+        self.0 = val;
+    }
+}
+impl Default for Ver {
+    #[inline(always)]
+    fn default() -> Ver {
+        Ver(0)
+    }
+}
+impl core::fmt::Debug for Ver {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        f.debug_struct("Ver")
+            .field("id", &format_args!("0x{:08x}", self.id()))
+            .finish()
+    }
+}
+#[cfg(feature = "defmt")]
+impl defmt::Format for Ver {
+    fn format(&self, f: defmt::Formatter) {
+        defmt::write!(f, "Ver {{ id: {=u32:#010x} }}", self.id());
+    }
+}
